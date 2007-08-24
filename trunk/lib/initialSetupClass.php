9d506cd137fcb53a5b1b8fe3cd4fd57d9ccc75f5
crazedsanity <crazedsanity@d017b47b-7037-0410-bb4d-9d1665c52a66>
2007-08-24 16:13:19 +0000

    Files that accidentally weren't added in last commit.

---
+++ trunk/lib/initialSetupClass.php
@@ -0,0 +1,16 @@
+<?php
+/*
+ * Created on May 11, 2007
+ * 
+ * SVN INFORMATION:::
+ * SVN Signature: $Id$
+ * Last Committted Date: $Date$
+ * Last Committed Path: $HeadURL$
+ */
+
+
+class initialSetup {
+	
+	//
+}
+?>
