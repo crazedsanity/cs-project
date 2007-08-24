6cdcc5f351a9b6a10386dd0b8c8a05ad651cfcd9
crazedsanity <crazedsanity@d017b47b-7037-0410-bb4d-9d1665c52a66>
2007-08-24 17:20:27 +0000

    Fix links to cs-content, plus public_html/setup.php is now a link to public_html/setup

--- trunk/lib/genericPageClass.php
+++ trunk/lib/genericPageClass.php
@@ -1,12 +1,12 @@
 <?php
 /*
  * SVN INFORMATION:::
- * SVN Signature: $Id$
- * Last Committted Date: $Date$
- * Last Committed Path: $HeadURL$
+ * SVN Signature: $Id:genericPageClass.php 8 2007-08-23 23:22:35Z crazedsanity $
+ * Last Committted Date: $Date:2007-08-23 18:22:35 -0500 (Thu, 23 Aug 2007) $
+ * Last Committed Path: $HeadURL:https://cs-project.svn.sourceforge.net/svnroot/cs-project/trunk/lib/genericPageClass.php $
  */
 require_once("includes.php");
-require_once(dirname(__FILE__) ."/externals/cs-content/contentSystemClass.php");//includes all the files for cs-content...
+require_once(dirname(__FILE__) ."/cs-content/contentSystemClass.php");//includes all the files for cs-content...
 
 class GenericPage extends cs_genericPage
 	{
