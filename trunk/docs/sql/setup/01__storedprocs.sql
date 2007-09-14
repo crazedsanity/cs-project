bd9e2d9bcc01c0ed92a4c78d9017376141b25591
crazedsanity <crazedsanity@d017b47b-7037-0410-bb4d-9d1665c52a66>
2007-09-14 23:17:47 +0000

    Separate the SQL headers (connect settings) & plpgsql stuff from the regular SQL... maybe ask if pl/pgsql is installed?  Or (preferrably) test for it?

--- trunk/docs/sql/setup/01__storedprocs.sql
+++ trunk/docs/sql/setup/01__storedprocs.sql
@@ -4,50 +4,9 @@
 -- Last Committted Date: $Date$
 -- Last Committed Path: $HeadURL$
 --
-SET client_encoding = 'SQL_ASCII';
-SET check_function_bodies = false;
-SET client_min_messages = warning;
-
---
--- Name: SCHEMA public; Type: COMMENT; Schema: -; Owner: postgres
---
-
-COMMENT ON SCHEMA public IS 'Standard public schema';
 
 
-SET search_path = public, pg_catalog;
 
---
--- Name: plpgsql_call_handler(); Type: FUNCTION; Schema: public; Owner: postgres
---
-
-CREATE FUNCTION plpgsql_call_handler() RETURNS language_handler
-    AS '$libdir/plpgsql', 'plpgsql_call_handler'
-    LANGUAGE c;
-
-
-ALTER FUNCTION public.plpgsql_call_handler() OWNER TO postgres;
-
---
--- Name: plpgsql_validator(oid); Type: FUNCTION; Schema: public; Owner: postgres
---
-
-CREATE FUNCTION plpgsql_validator(oid) RETURNS void
-    AS '$libdir/plpgsql', 'plpgsql_validator'
-    LANGUAGE c;
-
-
-ALTER FUNCTION public.plpgsql_validator(oid) OWNER TO postgres;
-
---
--- Name: plpgsql; Type: PROCEDURAL LANGUAGE; Schema: public; Owner: 
---
-
-CREATE TRUSTED PROCEDURAL LANGUAGE plpgsql HANDLER plpgsql_call_handler VALIDATOR plpgsql_validator;
-
---
--- Name: attribute_get_create(text); Type: FUNCTION; Schema: public; Owner: postgres
---
 
 CREATE FUNCTION attribute_get_create(text) RETURNS integer
     AS $_$
