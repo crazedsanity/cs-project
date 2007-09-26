0691ea472b0269b420f0ccb7f4bee604071e187d
crazedsanity <crazedsanity@d017b47b-7037-0410-bb4d-9d1665c52a66>
2007-09-26 18:49:28 +0000

    Code formatting.

--- trunk/lib/bbCode/bbCodeParser.class.php
+++ trunk/lib/bbCode/bbCodeParser.class.php
@@ -19,60 +19,56 @@
  */
 
 class bbCodeParser {
-
-function verifyBBCode( $data echo) {
- 
-	$data = str_replace("\n", '\newline\\', $data); 
- 
-	# Which BBCode is accepted here
-	$this->bbcodedata = array(
 	
-	 'bold' => array(
-	  'start' => array('[b]', '\[b\](.*)', '<b>\\1'),
-	  'end' => array('[/b]', '\[\/b\]', '</b>'),
-	 ),
-	 
-	 'underline' => array(
-	  'start' => array('[u]', '\[u\](.*)', '<u>\\1'),
-	  'end' => array('[/u]', '\[\/u\]', '</u>'),
-	 ),
-	 
-	 'italic' => array(
-	  'start' => array('[i]', '\[i\](.*)', '<i>\\1'),
-	  'end' => array('[/i]', '\[\/i\]', '</i>'),
-	 ),
-	 
-	 'image' => array(
-	  'start' => array('[img]', '\[img\](http:\/\/|ftp:\/\/)(.*)(.jpg|.jpeg|.bmp|.gif|.png)', '<img src=\'\\1\\2\\3\' />'),
-	  'end' => array('[/img]', '\[\/img\]', ''), 
-	 ),
-	 
-	 'url1' => array(
-	  'start' => array('[url]', '\[url\](http:\/\/|ftp:\/\/)(.*)', '<a href=\'\\1\\2\'>\\1\\2'),
-	  'end' => array('[/url]', '\[\/url\]', '</a>'),
-	 ),
-	 
-	 'url2' => array(
-	  'start' => array('[url]', '\[url=(http:\/\/|ftp:\/\/)(.*)\](.*)', '<a href=\'\\1\\2\'>\\3'), 
-	  'end' => array('[/url]', '\[\/url\]', '</a>'),
-	 ),
-	 
-	 'code' => array(
-	  'start' => array('[code]', '\[code\](.*)', 'CODE : <br /><div id="code">\\1'),
-	  'end' => array('[/code]', '\[\/code\]', '</div>'),
-	 ),
-	 
-	);
+	function verifyBBCode($data) {
+		$data = str_replace("\n", '\newline\\', $data); 
 	
-	foreach( $this->bbcodedata as $k => $v )
-	 {
-	   $data = preg_replace("#".$this->bbcodedata[$k]['start'][1].$this->bbcodedata[$k]['end'][1]."#", $this->bbcodedata[$k]['start'][2].$this->bbcodedata[$k]['end'][2], $data);
-	 }
-	 
-	$data = str_replace('\newline\\', '<br />', $data); 
-	 
-	 return $data;
- }
+		# Which BBCode is accepted here
+		$this->bbcodedata = array(
+			'bold' => array(
+				'start'	=> array('[b]', '\[b\](.*)', '<b>\\1'),
+				'end'	=> array('[/b]', '\[\/b\]', '</b>'),
+			),
+			
+			'underline' => array(
+				'start'	=> array('[u]', '\[u\](.*)', '<u>\\1'),
+				'end'	=> array('[/u]', '\[\/u\]', '</u>'),
+			),
+			
+			'italic' => array(
+				'start'	=> array('[i]', '\[i\](.*)', '<i>\\1'),
+				'end'	=> array('[/i]', '\[\/i\]', '</i>'),
+			),
+			
+			'image' => array(
+				'start'	=> array('[img]', '\[img\](http:\/\/|ftp:\/\/)(.*)(.jpg|.jpeg|.bmp|.gif|.png)', '<img src=\'\\1\\2\\3\' />'),
+				'end'	=> array('[/img]', '\[\/img\]', ''), 
+			),
+			
+			'url1' => array(
+				'start'	=> array('[url]', '\[url\](http:\/\/|ftp:\/\/)(.*)', '<a href=\'\\1\\2\'>\\1\\2'),
+				'end'	=> array('[/url]', '\[\/url\]', '</a>'),
+			),
+			
+			'url2' => array(
+				'start'	=> array('[url]', '\[url=(http:\/\/|ftp:\/\/)(.*)\](.*)', '<a href=\'\\1\\2\'>\\3'), 
+				'end'	=> array('[/url]', '\[\/url\]', '</a>'),
+			),
+			
+			'code' => array(
+				'start'	=> array('[code]', '\[code\](.*)', 'CODE : <br /><div id="code">\\1'),
+				'end'	=> array('[/code]', '\[\/code\]', '</div>'),
+			),
+		);
+		
+		foreach( $this->bbcodedata as $k => $v ) {
+			$data = preg_replace("#".$this->bbcodedata[$k]['start'][1].$this->bbcodedata[$k]['end'][1]."#", $this->bbcodedata[$k]['start'][2].$this->bbcodedata[$k]['end'][2], $data);
+		}
+		
+		$data = str_replace('\newline\\', '<br />', $data); 
+		
+		return $data;
+	}//end 
 	
 }
 ?>
