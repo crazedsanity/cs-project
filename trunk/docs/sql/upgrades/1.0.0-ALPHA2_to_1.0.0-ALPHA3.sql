d11a0053d54a2ce71115e885e70d23a74e1dae5c
crazedsanity <crazedsanity@d017b47b-7037-0410-bb4d-9d1665c52a66>
2007-09-28 20:40:59 +0000

    Consolidate all the SQL into a single file for sanity... ;)

--- trunk/docs/sql/upgrades/1.0.0-ALPHA2_to_1.0.0-ALPHA3.sql
+++ trunk/docs/sql/upgrades/1.0.0-ALPHA2_to_1.0.0-ALPHA3.sql
@@ -40,3 +40,41 @@ CREATE TABLE project_note_table (
     creator_contact_id integer NOT NULL,
     project_id integer NOT NULL REFERENCES project_table(project_id)
 );
+
+
+CREATE TABLE helpdesk_table (
+	helpdesk_id serial NOT NULL PRIMARY KEY,
+    group_id integer,
+    creator_contact_id integer,
+    leader_contact_id integer,
+    status_id integer DEFAULT 0 NOT NULL,
+    priority smallint,
+    progress smallint DEFAULT 0 NOT NULL,
+    start_date timestamp without time zone DEFAULT NOW(),
+    deadline date,
+    last_updated timestamp without time zone DEFAULT now(),
+    title text NOT NULL,
+    body text NOT NULL,
+	project_id integer REFERENCES project_table(project_id),
+    is_internal_only boolean DEFAULT false NOT NULL
+);
+
+
+
+CREATE TABLE helpdesk_note_table (
+    helpdesk_note_id serial NOT NULL,
+    title text NOT NULL,
+    body text NOT NULL,
+    created timestamp with time zone DEFAULT now() NOT NULL,
+    updated timestamp with time zone,
+    creator_contact_id integer NOT NULL,
+    helpdesk_id integer NOT NULL REFERENCES helpdesk_table(helpdesk_id),
+    is_solution boolean DEFAULT false
+);
+
+
+ALTER TABLE todo_table RENAME COLUMN record_id TO project_id;
+
+ALTER TABLE ONLY todo_table
+    ADD CONSTRAINT todo_table_project_id_fkey FOREIGN KEY (project_id) REFERENCES project_table(project_id);
+
