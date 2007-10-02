c35685369d59a3add1a0073aa2e073257c3a9218
crazedsanity <crazedsanity@d017b47b-7037-0410-bb4d-9d1665c52a66>
2007-10-02 14:41:23 +0000

    Implementation of the first specialized code for bbCode parsing.
    /lib/bbCode/bbCodeParser.class.php:
    	* MAIN:::
    		-- now it's just an extension of the bbCode class from cs-content.
    	* __construct():
    		-- now requires a projectClass{} and helpdeskClass{} object to be passed 
    		so they can be used for special bbCode processing/parsing.
    		-- calls parent::__construct().
    		-- registers the "[project_id={n}]" bbCode.
    	* get_project_bbcode() [NEW]:
    		-- returns a string from the given projectId (callback for the special 
    		bbCode parsing).
    	* parseString() [DELETED]:
    		-- now in cs_bbCodeParser{}.

--- trunk/lib/bbCode/bbCodeParser.class.php
+++ trunk/lib/bbCode/bbCodeParser.class.php
@@ -18,7 +18,9 @@
  * been converted.
  */
 
-class bbCodeParser {
+require_once(dirname(__FILE__) .'/../cs-content/cs_bbCodeParser.class.php');
+
+class bbCodeParser extends cs_bbCodeParser {
 	
 	/** Array containing all the codes & how to parse them. */
 	private $bbCodeData = NULL;
@@ -27,86 +29,25 @@ class bbCodeParser {
 	/**
 	 * Setup internal structures.
 	 */
-	function __construct() {
-		# Which BBCode is accepted here
-		$this->bbCodeData = array(
-			'bold' => array(
-				'start'	=> array('[b]', '\[b\](.*)', '<b>\\1'),
-				'end'	=> array('[/b]', '\[\/b\]', '</b>'),
-			),
-			
-			'underline' => array(
-				'start'	=> array('[u]', '\[u\](.*)', '<u>\\1'),
-				'end'	=> array('[/u]', '\[\/u\]', '</u>'),
-			),
-			
-			'italic' => array(
-				'start'	=> array('[i]', '\[i\](.*)', '<i>\\1'),
-				'end'	=> array('[/i]', '\[\/i\]', '</i>'),
-			),
-			
-			'image' => array(
-				'start'	=> array('[img]', '\[img\](http:\/\/|ftp:\/\/|\/)(.*)(.jpg|.jpeg|.bmp|.gif|.png)', '<img src=\'\\1\\2\\3\' />'),
-				'end'	=> array('[/img]', '\[\/img\]', ''), 
-			),
-			
-			#  [url]http://x.com[/url]
-			'url1' => array(
-				'start'	=> array('[url]', '\[url\](http:\/\/|ftp:\/\/)(.*)', '<a target="_blank" href=\'\\1\\2\'>\\1\\2'),
-				'end'	=> array('[/url]', '\[\/url\]', '</a>'),
-			),
-			
-			# [url=http://x.com]stuff[/url]
-			'url2' => array(
-				'start'	=> array('[url]', '\[url=(http:\/\/|ftp:\/\/)(.*)\](.*)', '<a target="_blank" href=\'\\1\\2\'>\\3'), 
-				'end'	=> array('[/url]', '\[\/url\]', '</a>'),
-			),
-			
-			'code' => array(
-				'start'	=> array('[code]', '\[code\](.*)', '<br /><br /><b>CODE</b>:<div class="code">\\1'),
-				'end'	=> array('[/code]', '\[\/code\]', '</div><br />'),
-			),
-		);
+	function __construct(projectClass $proj, helpdeskClass $helpdesk) {
+		$this->projectObj = $proj;
+		$this->helpdeskObj = $helpdesk;
+		
+		parent::__construct();
+		
+		//register some extra parsing things.
+		
+		$this->register_code_with_callback('project_id', 'get_project_bbcode');
 	}//end __construct()
 	//=========================================================================
 	
 	
 	
 	//=========================================================================
-	/**
-	 * Ensure the object is initialized properly, throw exception if not.
-	 */
-	private function isInitialized() {
-		if(!is_array($this->bbCodeData) || !count($this->bbCodeData)) {
-			throw new exception(__METHOD__ .": BBCode array not initialized");
-		}
-	}//end isInitialized()
-	//=========================================================================
-	
-	
-	
-	//=========================================================================
-	/**
-	 * Parse BBCode from the given string & return it with formatting.
-	 */
-	function parseString($data, $newlines2BR=FALSE) {
-		if(is_string($data) && strlen($data) > 10) {
-			$this->isInitialized();
-			$data = str_replace("\n", '||newline||', $data); 
-			
-			foreach( $this->bbCodeData as $k => $v ) {
-				$data = preg_replace("/".$this->bbCodeData[$k]['start'][1].$this->bbCodeData[$k]['end'][1]."/U", $this->bbCodeData[$k]['start'][2].$this->bbCodeData[$k]['end'][2], $data);
-			}
-			
-			$replaceNewlineStr = "\n";
-			if($newlines2BR) {
-				$replaceNewlineStr = "<br />\n";
-			}
-			$data = str_replace('||newline||', $replaceNewlineStr, $data); 
-			
-		}
-		return $data;
-	}//end parseString()
+	protected function get_project_bbcode($projectId) {
+		$retval = '&#91;<div style="display:inline;">'. $this->projectObj->get_ancestry_link_list($projectId, TRUE, TRUE) .'</div>&#93;';
+		return($retval);
+	}//end get_project_bbcode();
 	//=========================================================================
 	
 }
