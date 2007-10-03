d16713f76db5102977641a8aaf2f6ce37f550100
crazedsanity <crazedsanity@d017b47b-7037-0410-bb4d-9d1665c52a66>
2007-10-03 18:49:59 +0000

    Scripts & templates for attempting to convert database...

--- branches/setup_import_data/public_html/js/cs-project.js
+++ branches/setup_import_data/public_html/js/cs-project.js
@@ -36,3 +36,39 @@ function enableInput(prefixName) {
 	//make the input div appear.
 	toggleDisplay(inputDivName, 'inline');
 }
+
+/**
+ * Hide the text div & enable one of two divs.
+**/
+function setup__enableInput(prefixName, selectedOption) {
+
+	//EXAMPLE: if "prefixName"=="example" and "selectedOption"=="option1"...
+	
+	//example_text
+	var textDivName			= prefixName + '_text';
+	
+	//example_option1_div
+	var optionDivName		= prefixName + '_' + selectedOption + '_div';
+	
+	//example_option1_submitButton
+	var submitButtonName	= prefixName + '_' + selectedOption +'_submitButton';
+	
+	//example_selectedOption
+	var selectedOptionName	= prefixName + '_selectedOption';
+	
+	//hide the text.
+	var textDiv = document.getElementById(textDivName);
+	textDiv.style.display = 'none';
+	
+	//display the option.
+	var optionDiv = document.getElementById(optionDivName);
+	optionDiv.style.display = 'inline';
+	
+	//set the value of the option input.
+	var selectedOptionInput = document.getElementById(selectedOptionName);
+	selectedOptionInput.value = selectedOption;
+	
+	//display the submit button.
+	var submitButton = document.getElementById(submitButtonName);
+	submitButton.style.display = 'inline';
+}
