6d992014d0069ff8687f6730242ef8656dd54319
crazedsanity <crazedsanity@d017b47b-7037-0410-bb4d-9d1665c52a66>
2007-10-03 15:41:34 +0000

    Prepare for 1.0.0-BETA2

---
+++ releases/1.0.0-BETA2/docs/sql/setup/plpgsql.sql
@@ -0,0 +1,38 @@
+--
+-- SVN INFORMATION:::
+-- SVN Signature: $Id$
+-- Last Committted Date: $Date$
+-- Last Committed Path: $HeadURL$
+--
+
+--
+-- Name: plpgsql_call_handler(); Type: FUNCTION; Schema: public; Owner: postgres
+--
+
+CREATE FUNCTION plpgsql_call_handler() RETURNS language_handler
+    AS '$libdir/plpgsql', 'plpgsql_call_handler'
+    LANGUAGE c;
+
+
+ALTER FUNCTION public.plpgsql_call_handler() OWNER TO postgres;
+
+--
+-- Name: plpgsql_validator(oid); Type: FUNCTION; Schema: public; Owner: postgres
+--
+
+CREATE FUNCTION plpgsql_validator(oid) RETURNS void
+    AS '$libdir/plpgsql', 'plpgsql_validator'
+    LANGUAGE c;
+
+
+ALTER FUNCTION public.plpgsql_validator(oid) OWNER TO postgres;
+
+--
+-- Name: plpgsql; Type: PROCEDURAL LANGUAGE; Schema: public; Owner: 
+--
+
+CREATE TRUSTED PROCEDURAL LANGUAGE plpgsql HANDLER plpgsql_call_handler VALIDATOR plpgsql_validator;
+
+--
+-- Name: attribute_get_create(text); Type: FUNCTION; Schema: public; Owner: postgres
+--
