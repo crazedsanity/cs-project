586bc6f5c94ef0a939e97bb2a1b78658f3fe6abe
crazedsanity <crazedsanity@d017b47b-7037-0410-bb4d-9d1665c52a66>
2007-10-30 04:54:11 +0000

    Static export of external libraries for 1.1:
     * cs-content: 0.10.0
     * cs-arrayToPath: 0.2.2
     * cs-phpxml: 0.5.4

---
+++ releases/1.1/lib/cs-content/cs_sessionClass.php
@@ -0,0 +1,51 @@
+<?php
+/*
+ * FILE INFORMATION:
+ * $HeadURL: https://cs-content.svn.sourceforge.net/svnroot/cs-content/releases/0.10/cs_sessionClass.php $
+ * $Id: cs_sessionClass.php 161 2007-09-19 02:49:28Z crazedsanity $
+ * $LastChangedDate: 2007-09-18 21:49:28 -0500 (Tue, 18 Sep 2007) $
+ * $LastChangedBy: crazedsanity $
+ * $LastChangedRevision: 161 $
+ */
+
+require_once(dirname(__FILE__) ."/cs_versionAbstract.class.php");
+
+class cs_session extends cs_versionAbstract {
+
+	protected $db;
+	public $uid;
+	public $sid;
+	public $sid_check = 1;
+	
+	//---------------------------------------------------------------------------------------------
+	function __construct($createSession=1) {
+		if($createSession) {
+			//now actually create the session.
+			session_start();
+		}
+		
+		//check if there's a uid in the session already.
+		//TODO: need a setting somewhere that says what the name of this var should be,
+		//	instead of always forcing "uid".
+		$this->uid = 0;
+		if($_SESSION['uid']) {
+			$this->uid = $_SESSION['uid'];
+		}
+		
+		//grab what our session_id is...
+		$this->sid = session_id();
+		
+	}//end __construct()
+	//---------------------------------------------------------------------------------------------
+	
+	
+	
+	//---------------------------------------------------------------------------------------------
+	public function is_authenticated() {
+		return(FALSE);
+	}//end is_authenticated()
+	//---------------------------------------------------------------------------------------------
+
+
+}//end cs_session{}
+?>
