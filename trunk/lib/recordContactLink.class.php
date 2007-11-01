acf6b64839fc2749d11a8649652b7cc9b95bb300
crazedsanity <crazedsanity@d017b47b-7037-0410-bb4d-9d1665c52a66>
2007-11-01 16:20:08 +0000

    Okay, last commit just cleaned up some separator lines in projectClass.  This one is the beginnings of record->contact associations.  ;)

---
+++ trunk/lib/recordContactLink.class.php
@@ -0,0 +1,27 @@
+<?php
+/*
+ * Created on November 1st, 2007
+ * 
+ * SVN INFORMATION:::
+ * ------------------
+ * SVN Signature::::::: $Id$
+ * Last Author::::::::: $Author$ 
+ * Current Revision:::: $Revision$ 
+ * Repository Location: $HeadURL$ 
+ * Last Updated:::::::: $Date$
+ */
+
+
+class recordContactLink extends dbAbstract {
+	
+	public $db;
+	protected $gfObj;
+	
+	//=========================================================================
+	function __construct() {
+	}//end __construct()
+	//=========================================================================
+	
+	
+}
+?>
