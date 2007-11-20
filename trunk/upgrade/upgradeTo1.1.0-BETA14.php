fafc4ac33d86074d6a32b153f8e72744fbcd1fce
crazedsanity <crazedsanity@d017b47b-7037-0410-bb4d-9d1665c52a66>
2007-11-20 18:31:53 +0000

    Upgrade script for fixing a problematic constraint.

---
+++ trunk/upgrade/upgradeTo1.1.0-BETA14.php
@@ -0,0 +1,55 @@
+<?php
+/*
+ * Created on Oct 29, 2007
+ */
+
+
+class upgrade_to_1_1_0_BETA13 extends dbAbstract {
+	
+	private $logsObj;
+	
+	//=========================================================================
+	public function __construct(cs_phpDB &$db) {
+		if(!$db->is_connected()) {
+			throw new exception(__METHOD__ .": database is not connected");
+		}
+		$this->db = $db;
+		
+		$this->logsObj = new logsClass($this->db, 'Upgrade');
+		
+		$this->gfObj = new cs_globalFunctions;
+		$this->gfObj->debugPrintOpt = DEBUGPRINTOPT;
+	}//end __construct()
+	//=========================================================================
+	
+	
+	
+	//=========================================================================
+	public function run_upgrade() {
+		
+		
+		$this->db->beginTrans(__METHOD__);
+		
+		$this->run_schema_changes();
+		
+		$this->db->commitTrans(__METHOD__);
+		
+	}//end run_upgrade()
+	//=========================================================================
+	
+	
+	
+	//=========================================================================
+	private function run_schema_changes() {
+		
+		$this->gfObj->debug_print(__METHOD__ .": running SQL file...");
+		$this->run_sql_file(dirname(__FILE__) .'/../docs/sql/upgrades/upgradeTo1.1.0-BETA14.sql');
+		
+		$details = "Executed SQL file, '". $this->lastSQLFile ."'.  Encoded contents::: ". 
+			base64_encode($this->fsObj->read($this->lastSQLFile));
+		$this->logsObj->log_by_class($details, 'system');
+	}//end run_schema_changes()
+	//=========================================================================
+}
+
+?>
