d1910f37ed0dd72d9f077f14eb64e004e127c536
crazedsanity <crazedsanity@d017b47b-7037-0410-bb4d-9d1665c52a66>
2007-12-13 23:41:02 +0000

    Script to implement invoice stuff in next version...

---
+++ trunk/docs/sql/upgrades/upgradeTo1.1.0-BETA16.sql
@@ -0,0 +1,53 @@
+--
+-- SVN INFORMATION:::
+-- 
+-- SVN Signature::::::::: $Id$
+-- Last Committted Date:: $Date$
+-- Last Committed Path::: $HeadURL$
+--
+
+
+CREATE TABLE invoice_status_table (
+	invoice_status_id integer NOT NULL PRIMARY KEY,
+	name text NOT NULL UNIQUE,
+	is_updateable boolean NOT NULL DEFAULT FALSE
+);
+
+CREATE TABLE invoice_table (
+    invoice_id integer NOT NULL PRIMARY KEY,
+	poc text,
+    company text,
+	address1 text,
+	address2 text,
+	phone text,
+	fax text,
+	city text,
+	state text,
+	zip text,
+	invoice_status_id integer NOT NULL REFERENCES invoice_status_table(invoice_status_id),
+	creator_contact_id integer NOT NULL REFERENCES contact_table(contact_id),
+	billing_contact_id integer NOT NULL REFERENCES contact_table(contact_id),
+	is_proforma boolean NOT NULL DEFAULT FALSE,
+	date_created date NOT NULL DEFAULT CURRENT_DATE
+);
+
+
+CREATE TABLE invoice_item_table (
+	invoice_item_id integer NOT NULL PRIMARY KEY,
+	invoice_id integer NOT NULL REFERENCES invoice_table(invoice_id),
+	description text NOT NULL,
+	unit_price decimal(10,2),
+	quantity integer NOT NULL DEFAULT 1
+);
+
+
+
+CREATE TABLE invoice_transaction_table (
+	invoice_transaction_id integer NOT NULL PRIMARY KEY,
+	invoice_id integer NOT NULL REFERENCES invoice_table(invoice_id),
+	auth_string text NOT NULL,
+	number text NOT NULL,
+	date_created date NOT NULL DEFAULT CURRENT_DATE
+);
+
+
