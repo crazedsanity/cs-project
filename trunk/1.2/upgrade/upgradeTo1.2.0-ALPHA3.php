21c7d10bddbfdfdb5ae7bf1f973c6ed3b55e55e9
crazedsanity <crazedsanity@d017b47b-7037-0410-bb4d-9d1665c52a66>
2008-02-09 05:34:13 +0000

    Beginning of re-writing XML config, but HTML encoding is a bitch (issue #150)

--- trunk/1.2/upgrade/upgradeTo1.2.0-ALPHA3.php
+++ trunk/1.2/upgrade/upgradeTo1.2.0-ALPHA3.php
@@ -1,6 +1,6 @@
 <?php
 /*
- * Created on Nov 20, 2007
+ * Created on February 08, 2008
  */
 
 
@@ -18,7 +18,7 @@ class upgrade_to_1_2_0_ALPHA3 extends dbAbstract {
 		$this->logsObj = new logsClass($this->db, 'Upgrade');
 		
 		$this->gfObj = new cs_globalFunctions;
-		$this->gfObj->debugPrintOpt = DEBUGPRINTOPT;
+		$this->gfObj->debugPrintOpt = 1;
 	}//end __construct()
 	//=========================================================================
 	
@@ -27,15 +27,47 @@ class upgrade_to_1_2_0_ALPHA3 extends dbAbstract {
 	//=========================================================================
 	public function run_upgrade() {
 		
-		
 		$this->db->beginTrans(__METHOD__);
 		
-		
+		$this->update_config_file();
 		exit;
 		
 		$this->db->commitTrans(__METHOD__);
 		
 	}//end run_upgrade()
+	//=========================================================================
+	
+	
+	
+	//=========================================================================
+	public function update_config_file() {
+		$fs = new cs_fileSystemClass(dirname(__FILE__) .'/../');
+		$sampleXmlObj = new XMLParser($fs->read('docs/samples/sample_config.xml'));
+		$siteXmlObj = new XMLParser($fs->read('lib/config.xml'));
+		
+		$updateXml = new xmlCreator();
+		$updateXml->load_xmlparser_data($siteXmlObj);
+		
+		$sampleIndexes = $sampleXmlObj->get_tree(TRUE);
+		$sampleIndexes = $sampleIndexes['CONFIG'];
+		$siteConfigIndexes = $siteXmlObj->get_tree(TRUE);
+		
+		foreach($sampleIndexes as $indexName=>$indexValue) {
+			$path = '/CONFIG/'. $indexName;
+			$attributes = $sampleXmlObj->get_attribute($path);
+			#debug_print(__METHOD__ .": attributes from sample (/CONFIG/". $indexName ."::: ",1);
+			#debug_print($attributes,1);
+			
+			if(is_array($attributes)) {
+				$updateXml->add_attribute($path, $attributes);
+			}
+		}
+		
+		$this->gfObj->debug_print($updateXml->create_xml_string());
+		$fs->openFile('lib/_test_config.xml');
+		$fs->write($updateXml->create_xml_string());
+		exit;
+	}//end update_config_file()
 	//=========================================================================
 }
 
